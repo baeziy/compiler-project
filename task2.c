aaf5b76d736f3f48ad757bec33a836fed8b68e1f
Mustafa Kamal <mustfakmalik@gmail.com>
2023-06-04 19:01:41 +0500

    make task1.c and task2.c

---
+++ task2.c
@@ -0,0 +1,22 @@
+/*
+              DATA DICTIONARY
+              COMP 451 Section X
+              Spring 2023
+              Project
+              Group Members: Muhammad Mustafa Kamal Malik
+                             Mahnoor
+              Roll Number:   241610047
+                             231450813
+              Date of Submission: June 04, 2023
+*/
+
+
+#include<stdio.h>
+
+int main(int argc, char ** argv){
+
+    
+
+
+    return 0;
+}
