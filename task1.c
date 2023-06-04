b7e7a475a3bf4d0a6379ca3f44815440d8c9760f
Mustafa Kamal <mustfakmalik@gmail.com>
2023-06-04 20:14:49 +0500

    test0 for task1.c

--- task1.c
+++ task1.c
@@ -71,7 +71,7 @@ int main(int argc, char ** argv){
         }
 
         if(!sen)
-            printf("Sentinal character not found for: %s\n", input);
+            printf("Sentinal character not found for: %s %d\n", input, sen);
 
 
     }
